2e1e573b7c08497b3efa9367a8c408bbb8333d30
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2017-02-28 15:50:56 +1300

    Remove extraneous documentation comments

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,11 +3,6 @@ import classnames from 'classnames';
 
 import styles from './button.css';
 
-/**
- * Button Component
- * @class
- * @version 0.1
- */
 const Button = ({ className, onClick, disabled, children, ...props }) =>
   <button
     className={classnames(className, styles.button, { [styles.disabled]: disabled })}
@@ -18,13 +13,6 @@ const Button = ({ className, onClick, disabled, children, ...props }) =>
     {children}
   </button>;
 
-/**
- * @property {object}     propTypes                   - Button PropTypes
- * @property {string}     propTypes.className         - Input class name for the button, defaults to styles.button
- * @property {function}   propTypes.onClick           - On Click handler
- * @property {boolean}    propTypes.disabled          - Disabled flag
- * @property {node}       propTypes.children          - Child node(s)
- */
 Button.propTypes = {
   className: PropTypes.string,
   onClick: PropTypes.func.isRequired,
