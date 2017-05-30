6078f22f3c22e4c8b994344e8517e29870765a6f
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2017-05-31 10:33:46 +1200

    Fix flow and eslint

--- flow-typed/global.js
+++ flow-typed/global.js
@@ -4,8 +4,8 @@
 // For hot module loading - configureStore
 declare var module: {
   hot: {
-    accept(path: ?string) : void;
-  }
+    accept(path: ?string, cb: () => void): void,
+  },
 };
 
 // React helper types
