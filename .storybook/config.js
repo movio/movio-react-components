ac408e7f1e909be37b35e63cd06983b55c03f5c9
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2017-06-07 13:39:03 +1200

    Add decorator to center components

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,23 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
 
 const req = require.context('../src/components', true, /\.stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addDecorator(story => (
+  <div
+    style={{
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      transform: 'translate(-50%, -50%)',
+    }}
+  >
+    {story()}
+  </div>
+));
 
 configure(loadStories, module);
